c613a4090590afc307e1eaf82ec31dfd8c26ef26
Mario <Mario@LAPTOP-M0D7AUH5>
2021-03-17 15:04:53 +0100

    Clase DAO creada

---
+++ src/modelo/dao/TrabajadorbbddDAO.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo.dao;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import modelo.Categorias;
+import modelo.Empresas;
+import modelo.HibernateUtil;
+import modelo.Nomina;
+import modelo.Trabajadorbbdd;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author Mario
+ */
+public class TrabajadorbbddDAO {
+   
+
+public void buscarTrabajadorDNI(String dni){
+
+//Vamos a hacer un ejemplo en el main, pero no debería ser aqui sino en una clase DAO --> si un metodo consulta del trabajador por nif tendria un trabajadorDAO y ahi estaria el método
+        //los .java van al modelo, las clases dao van a la parte del modelo de acceso a datos
+        
+        //HibernateUtil me retorna un tipo SessionFactory; Necesitare 3 atributos, uno de tipo SessoinFactory, otro Session y otro Transaccion(para inserciones y eliminaciones) una query no necesita transacciones
+        //Acostumbrarse a usar try catch para capturar exceptions 
+        
+        SessionFactory sf = null;
+        Session session = null;
+        Transaction tx = null; //control+shift+i para importar librerias
+        
+        
+        try{
+            /*Pedimos a hibernate a traves de hibernate util que abra una conexión con la bd*/
+            sf = HibernateUtil.getSessionFactory();//metodo de HIbernateUtil
+            session = sf.openSession(); //abro la sesion con la base de datos
+            
+            
+            /*Voy a hacer una consulta; viene mucha mas informacion, no hago la consulta contra la tabla sino contra trabajadorbbdd.java; Respetar la T mayuscula de la clase
+            Ademas en hql no existe el Select, sino que me traigo un objeto completo, empezamos en from y la clase, no la tabla
+            */
+            
+           // String consultaHQL = "from Trabajadorbbdd t"; //dar un alias a la clase es obligatorio
+        /*Voy a consultar todos aquellos trabajadores cuyo NIF sea ese parámetro
+          No solo me estoy trayendo el nombre y el nif, o toda la info suya de la tabla trabajador, me estoy trayendo todas sus nóminas 
+           */
+           String consuta2HQL = "from Trabajadorbbdd t WHERE t.nifnie = :parametro"; //si quisiera mas seguiria con and :param2.... pasar parametros siempre, para evitar las inyecciones y nifnie es el atributo de la clase tal cual
+           
+           // Query query = session.createQuery(consultaHQL); //estoy creando la consulta, sobre la sesion abierta en hibernate
+            
+           Query query = session.createQuery(consuta2HQL); //aqui cuando compongo la consulta con el string de arriba, al tener un parámetro, tengo que dárselo a la query
+           query.setParameter("parametro", dni);//antes de ejecutarla le cargo el parametro
+           
+           List<Trabajadorbbdd> listaResultado = query.list(); //devuelve una lista de la base de datos sobre el objeto perdido, trabajadoresbbdd, en este caso retorna todos los trabajadores de la base de datos
+            
+            
+            //PRECONDICION: tengo toda la info de los trabajadores de la base de datos
+                
+                //Pinto de cada uno de ellos, su nombre y su NIF
+            for(int i = 0; i < listaResultado.size(); i++){
+                System.out.println("Nombre: " + listaResultado.get(i).getNombre());
+                System.out.println("NIF: " + listaResultado.get(i).getNifnie());
+                System.out.println("=============================================================");
+            }
+            
+            //Destruyo la sesion con Hibernate
+            HibernateUtil.shutdown();
+            
+            
+            
+        }catch(Exception e){
+            System.out.println("Ha ocurrido un error: " + e.getMessage());
+        }
+        
+        
+        
+
+
+}
+
+
+
+
+public Trabajadorbbdd recuperarDatosTrabajador(String dni){
+    
+        SessionFactory sf = null;
+        Session session = null;
+        Transaction tx = null;
+        Trabajadorbbdd trabajador = new Trabajadorbbdd(); 
+        Categorias categoria = new Categorias();
+        Empresas empresa = new Empresas();
+        Nomina nomina = new Nomina();
+        Set nominas = new HashSet(0);
+
+        try {
+            sf = HibernateUtil.getSessionFactory();
+            session = sf.openSession();
+
+            String consultaHQL = "FROM Trabajadorbbdd t WHERE t.nifnie = :param1";
+
+                   
+            //String dni = "10200147S";
+
+            Query query = session.createQuery(consultaHQL);
+            query.setParameter("param1", dni);
+
+            List<Trabajadorbbdd> listaResultado = query.list();
+
+            if (listaResultado.size() != 0) {
+
+                for (Trabajadorbbdd tbd : listaResultado) {
+                        //se rellena el trabajador pedido
+                    trabajador.setNombre(tbd.getNombre());
+                    trabajador.setApellido1(tbd.getApellido1());
+                    trabajador.setApellido2(tbd.getApellido2());
+                    trabajador.setNifnie(tbd.getNifnie());
+                    categoria.setNombreCategoria(tbd.getCategorias().getNombreCategoria());
+                    trabajador.setCategorias(categoria);
+                    empresa.setNombre(tbd.getEmpresas().getNombre());
+                    trabajador.setEmpresas(empresa);
+                    
+                    for (Iterator<Nomina> it = tbd.getNominas().iterator(); it.hasNext();) {
+                        Nomina n = it.next();
+                            //se rellena la nómina
+                        nomina.setMes(n.getMes());
+                        nomina.setAnio(n.getAnio());
+                        nomina.setBrutoNomina(n.getBrutoNomina());
+                            //se añade la nomina al set
+                        nominas.add(nomina);
+              
+                    }
+                        //se añade el conjunto de nóminas al trabajador buscado
+                    trabajador.setNominas(nominas);
+                    
+
+                }
+            }
+
+            HibernateUtil.shutdown();
+
+        } catch (Exception e) {
+            System.out.println("error");
+        }
+    return trabajador;
+}
+
+
+
+}
--- src/sistemas20202021/Sistemas20202021.java
+++ src/sistemas20202021/Sistemas20202021.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 import modelo.HibernateUtil;
 import modelo.Nomina;
 import modelo.Trabajadorbbdd;
+import modelo.dao.TrabajadorbbddDAO;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -28,56 +29,44 @@ public class Sistemas20202021 {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        SessionFactory sf = null;
-        Session session = null;
-        Transaction tx = null;
-
-        try {
-            sf = HibernateUtil.getSessionFactory();
-            session = sf.openSession();
-
-            String consultaHQL = "FROM Trabajadorbbdd t WHERE t.nifnie = :param1";
-
-            //pedir dni por consola
+        //pedir dni por consola
             Scanner teclado = new Scanner(System.in);
             System.out.println("Por favor, infroduzca el NIF del trabajador: ");
             String dni = teclado.nextLine();
+        
+        //llamada al metodo y control de si viene null que salte el error
+        TrabajadorbbddDAO camposTrabajador = new TrabajadorbbddDAO();
+        Trabajadorbbdd trabajadorEncontrado;
+        
+        trabajadorEncontrado = camposTrabajador.recuperarDatosTrabajador(dni);
+        
+        
+        if(trabajadorEncontrado != null){
             
-            //String dni = "10200147S";
-
-            Query query = session.createQuery(consultaHQL);
-
-            query.setParameter("param1", dni);
-
-            List<Trabajadorbbdd> listaResultado = query.list();
-
-            if (listaResultado.size() != 0) {
-
-                for (Trabajadorbbdd tbd : listaResultado) {
-                    System.out.println("Nombre: " + tbd.getNombre());
-                    System.out.println("Apellidos: " + tbd.getApellido1() + " " + tbd.getApellido2());
-                    System.out.println("NIF: " + tbd.getNifnie());
-                    System.out.println("Categoria: " + tbd.getCategorias().getNombreCategoria());
-                    System.out.println("Empresa: " + tbd.getEmpresas().getNombre());
-                    System.out.println("--");
-
-                    for (Iterator<Nomina> it = tbd.getNominas().iterator(); it.hasNext();) {
-                        Nomina n = it.next();
-                        System.out.println("Nomina: " + n.getMes() + "/" + n.getAnio() + " | " + n.getBrutoNomina() + "€");
-                    }
-
-                    System.out.println("-----------------------");
-
-                }
-            } else {
-                System.out.println("ERROR - El trabajador no esta en la base de datos");
+            System.out.println("Nombre: " + trabajadorEncontrado.getNombre());
+            System.out.println("Apellidos: " + trabajadorEncontrado.getApellido1() + " " + trabajadorEncontrado.getApellido2());
+            System.out.println("NIF: " + trabajadorEncontrado.getNifnie());
+            System.out.println("Categoria: " + trabajadorEncontrado.getCategorias().getNombreCategoria());
+            System.out.println("Empresa: " + trabajadorEncontrado.getEmpresas().getNombre());
+            System.out.println("--");
+        
+        
+        
+            for (Iterator<Nomina> it = trabajadorEncontrado.getNominas().iterator(); it.hasNext();) {
+                Nomina n = it.next();
+                System.out.println("Nomina: " + n.getMes() + "/" + n.getAnio() + " | " + n.getBrutoNomina() + "€");
             }
-
-            HibernateUtil.shutdown();
-
-        } catch (Exception e) {
-            System.out.println("error");
-        }
+        
+        
+            System.out.println("-----------------------");
+            
+        }else{
+            System.out.println("ERROR - El trabajador no esta en la base de datos");
+              
+        }                    
+                            
+                            
+        
     }
 
 }
